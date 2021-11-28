d2a8e4fa7db6a4625f61621fd9677b0b89b18c1f
yudai-simoma <cdcdasdll@icloud.com>
2021-11-28 18:40:23 +0900

    tweet機能実装準備

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,16 @@
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+  get 'tweets/new'
+  get 'tweets/create'
+  get 'tweets/show'
+  get 'tweets/delete'
   get 'homes/index'
   root to: "homes#index"
 end
